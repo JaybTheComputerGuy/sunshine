a267e229dcf663e502af9e61a4fb76358e6f890e
jayb21 <jayb2otieno@yahoo.com>
2015-05-09 00:36:17 +0300

    Find By view ID

--- app/src/main/java/com/example/jayb/sunshine2/MainActivityFragment.java
+++ app/src/main/java/com/example/jayb/sunshine2/MainActivityFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,6 +24,7 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main,container);
         String[] forecastArray = {
                 "Today - Sunny",
                 "Tommorow - Foggy",
@@ -35,9 +37,12 @@ public class MainActivityFragment extends Fragment {
 
         List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
 
-        ArrayAdapter<String> data_adapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,
+        ArrayAdapter<String> mforecastAdapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,
                 R.id.list_item_forecast_textView,weekForecast);
 
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        ListView listView = (ListView)rootView.findViewById(R.id.list_view_forecast);
+        listView.setAdapter(mforecastAdapter);
+
+        return rootView;
     }
 }
