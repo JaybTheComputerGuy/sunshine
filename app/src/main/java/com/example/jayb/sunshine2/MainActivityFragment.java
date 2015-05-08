74cc4e7717017cfc38c7684f18a8a8fa898330ca
jayb21 <jayb2otieno@yahoo.com>
2015-05-09 00:03:55 +0300

    Fake data created

--- app/src/main/java/com/example/jayb/sunshine2/MainActivityFragment.java
+++ app/src/main/java/com/example/jayb/sunshine2/MainActivityFragment.java
@@ -6,6 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 /**
  * A placeholder fragment containing a simple view.
@@ -18,6 +22,17 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String[] forecastArray = {
+                "Today - Sunny",
+                "Tommorow - Foggy",
+                "Weds - Cloudy",
+                "Thurs - Rainy",
+                "Friday - Cloudy",
+                "Sato - Foggy",
+                "Sun - Sunny"
+        };
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
