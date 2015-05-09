f13ac5ffda0d519ece971e2af9c84b810a73a13e
jayb21 <jayb2otieno@yahoo.com>
2015-05-09 22:55:46 +0300

    Network class added

--- app/src/main/java/com/example/jayb/sunshine2/MainActivityFragment.java
+++ app/src/main/java/com/example/jayb/sunshine2/MainActivityFragment.java
@@ -1,13 +1,21 @@
 package com.example.jayb.sunshine2;
 
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -24,6 +32,7 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.fragment_main,container);
         String[] forecastArray = {
                 "Today - Sunny",
@@ -43,6 +52,60 @@ public class MainActivityFragment extends Fragment {
         ListView listView = (ListView)rootView.findViewById(R.id.list_view_forecast);
         listView.setAdapter(mforecastAdapter);
 
+
+
         return rootView;
+    }
+
+    public class FetcWeatherTask extends AsyncTask<Void,Void,Void> {
+        private final String LOG_TAG = FetcWeatherTask.class.getSimpleName();
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String forecastJsonStr = null;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily/q=40441&mode=json&units=metric&cht=7");
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                if (inputStream == null) {
+                    return null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                return null;
+            }
+            finally{
+                if(urlConnection != null){
+                    urlConnection.disconnect();
+                }
+                if(reader != null){
+                    try{
+                        reader.close();
+                    }
+                    catch (final IOException e){
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            return null;
+         }
     }
 }
